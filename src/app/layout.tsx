01f04a89a9012498067567967033f1a28e4dee7f
Calliope <69815100+callioptsu@users.noreply.github.com>
2024-04-21 13:22:30 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["700"] });
+
+export const metadata: Metadata = {
+  title: "Next template",
+  description: "Template for front-end projects.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
