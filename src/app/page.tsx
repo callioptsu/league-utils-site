923a00d6a63a14b8b00902b2b2ed07496667b0ca
callioptsu <pasmac.dev@protonmail.com>
2024-04-22 13:56:04 -0300

    feat: add api fetch, actions and save informations in cookie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,51 @@
-import { riotApi } from "@/lib/utils/api";
+"use client";
+
+import { ChangeEvent, FormEvent, useState } from "react";
+import { fetchSummoner } from "@/lib/actions";
+import { ApiResponse } from "@/lib/interfaces";
+import Cookies from "js-cookie";
 
 /**
  * Development branch
  * branch/dev
  */
-export default async function Home() {
-  const data = await riotApi({options: "<option>", gameName: "<name>", tagLine: "<tag>", region: "<americas"});
-  console.log(data);
+export default function Home() {
+  const [info, setInfo] = useState({
+    region: "",
+    gameName: "",
+    tagLine: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+
+    setInfo((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data: ApiResponse = await fetchSummoner(info);
+
+    if (!data) {
+      return;
+    }
+
+    Cookies.set("summoner", JSON.stringify(data.summoner), {
+      expires: 1,
+    });
+  };
+
   return (
     <main>
-      <span>Home</span>
+      <form onSubmit={handleSubmit}>
+        <input id="region" placeholder="Region" onChange={handleChange} />
+        <input id="gameName" placeholder="Name" onChange={handleChange} />
+        <input id="tagLine" placeholder="#TagLine" onChange={handleChange} />
+        <button type="submit">Confirm</button>
+      </form>
     </main>
   );
 }
